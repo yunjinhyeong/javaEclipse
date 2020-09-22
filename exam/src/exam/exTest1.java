1d35fea08c8359443696ee0ff77af35085ebd7b6
yunjinhyeong <yju7257@naver.com>
2020-09-22 14:35:37 +0900

    first

---
+++ exam/src/exam/exTest1.java
@@ -0,0 +1,45 @@
+package exam;
+
+import java.sql.*;
+
+
+public class exTest1 {
+
+	public static void main(String[] args) {
+		// select문
+		// db데이터가져오기
+		// Test
+		String dbUrl = "jdbc:mysql://localhost:3306/sakila?useUnicode=true&characterEncoding=utf8&allowPublicKeyRetrieval=true&useSSL=false&serverTimezone=Asia/Seoul";
+		String dbId = "myid";
+		String dbPwd = "mypwd";
+		
+		//세팅하기
+		String sql = "";
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			con = DriverManager.getConnection(dbUrl, dbId, dbPwd);
+			sql = "Select * FROM actor where first_name LIKE 'B%'";
+			pstmt = con.prepareStatement(sql);
+			rs = pstmt.executeQuery(sql);
+			while(rs.next()) {
+				int actId = rs.getInt("actor_id");
+				String firstName = rs.getString("first_name");
+				String lastName = rs.getString("last_name");
+				Timestamp lastUpdate = rs.getTimestamp("last_update");
+				
+				System.out.print(actId + "\t");
+				System.out.print(firstName + "\t");
+				System.out.print(lastName + "\t");
+				System.out.println(lastUpdate);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	}
+
+}
