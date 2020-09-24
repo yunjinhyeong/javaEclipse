d6eb5680b45a8742103ad87763d8319922b9f809
yunjinhyeong <67212570+yunjinhyeong@users.noreply.github.com>
2020-09-24 09:54:48 +0900

    delete project1, project2

--- Project2/src/RandomClass.java
+++
@@ -1,8 +0,0 @@
-import java.util.Random;
-
-public class RandomClass {
-    public static void main(String[] args) {
-        Random random = new Random();
-        System.out.println(random.nextInt());
-    }
-}
