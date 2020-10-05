78b3ba6c77ca2636744b1d509a043f7ea2437787
yunjinhyeong <67212570+yunjinhyeong@users.noreply.github.com>
2020-10-05 11:29:51 +0900

    add 12EX

---
+++ 12EX/src/page3/paintJPanelEx.java
@@ -0,0 +1,37 @@
+package page3;
+
+import java.awt.Color;
+import java.awt.Graphics;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class paintJPanelEx extends JFrame {
+	private MyPanel panel = new MyPanel();
+	
+	public paintJPanelEx() {
+		setTitle("JPanel¿« paintComponent() øπ¡¶");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setContentPane(panel);
+		setSize(250, 220);
+		setVisible(true);
+	}
+	
+	class MyPanel extends JPanel {
+		
+		public void paintComponent(Graphics g) {		
+			super.paintComponent(g);
+			g.setColor(Color.BLUE);
+			g.drawRect(10, 10, 50, 50);
+			g.drawRect(50, 50, 50, 50);
+			g.setColor(Color.MAGENTA);
+			g.drawRect(90, 90, 50, 50);
+		}
+	}
+	
+	public static void main(String[] args) {
+		new paintJPanelEx();
+
+	}
+
+}
