260326b7bbaceebe02b904939e4bdb1c52bd36ce
yunjinhyeong <67212570+yunjinhyeong@users.noreply.github.com>
2020-10-05 10:50:34 +0900

    finish 2hour study

---
+++ java_study/src/java0924/Ex11.java
@@ -0,0 +1,28 @@
+package java0924;
+
+public class Ex11 {
+	public static void methodA(int a, int b, int c) {
+		
+	}
+	
+	public static void methodB(int... num) { // 가변인자 메소드
+		// 가변인자 메소드의 매개변수는 배열이 됨
+		System.out.println(num.length);
+		for(int n : num) {
+			System.out.print(n + "");
+		}
+		System.out.println("=========");
+		
+	}
+	
+
+	
+	public static void main(String[] args) {
+		methodA(10, 20, 30);
+		methodB();
+		methodB(100);
+		methodB(100, 200);
+
+	}
+
+}
