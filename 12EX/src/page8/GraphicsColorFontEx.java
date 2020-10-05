d17bd1b423059af639a9245e2a0ed1efb3bfafda
yunjinhyeong <67212570+yunjinhyeong@users.noreply.github.com>
2020-10-05 12:24:44 +0900

    no mention

---
+++ 12EX/src/page8/GraphicsColorFontEx.java
@@ -0,0 +1,13 @@
+package page8;
+
+import javax.swing.JFrame;
+
+public class GraphicsColorFontEx extends JFrame{
+	
+	
+	public static void main(String[] args) {
+		new GraphicsColorFontEx();
+
+	}
+
+}
