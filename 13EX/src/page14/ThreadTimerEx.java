f8f0c0374f4b649210669da68032571686e93fb9
yunjinhyeong <67212570+yunjinhyeong@users.noreply.github.com>
2020-10-06 09:24:40 +0900

    add 10 13 EX

---
+++ 13EX/src/page14/ThreadTimerEx.java
@@ -0,0 +1,10 @@
+package page14;
+
+public class ThreadTimerEx {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("asdf");
+	}
+
+}
---
+++ 13EX/src/page14/TimerThread.java
@@ -0,0 +1,5 @@
+package page14;
+
+public class TimerThread {
+	int a = 1;int b = 1;
+}
